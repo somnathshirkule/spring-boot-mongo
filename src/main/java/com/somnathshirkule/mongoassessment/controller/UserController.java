3dccdc0f6436d81a2c5bc0b2380e25b4f40da0aa
Somnath Shirkule <shirkule.somnath@gmail.com>
2023-02-10 17:35:55 +0530

    Update UserController.java

--- src/main/java/com/somnathshirkule/mongoassessment/controller/UserController.java
+++ src/main/java/com/somnathshirkule/mongoassessment/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.somnathshirkule.mongoassessment.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.somnathshirkule.mongoassessment.annotations.Validate;
+import com.somnathshirkule.mongoassessment.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,55 +12,49 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.somnathshirkule.mongoassessment.annotations.Validate;
-import com.somnathshirkule.mongoassessment.service.UserService;
-
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    /***
+     * This API used to save signup user details.
+     *
+     * @param jsonNode
+     * @return respective response msg.
+     */
+
+    @PostMapping("/signup")
+    @Validate(apiName = "signup")
+    public ResponseEntity<JsonNode> signUp(@RequestBody JsonNode jsonNode) {
+        log.debug("Entered into signUp");
+
+        JsonNode responseNode = userService.signUp(jsonNode);
+        if (log.isDebugEnabled()) {
+            log.debug("exited from signUp");
+        }
+        return new ResponseEntity<>(responseNode, HttpStatus.OK);
+    }
 	
-	@Autowired
-	UserService userService;
-	
-	/***
-	 * This API used to save signup user details.
-	 * 
-	 * @param jsonNode
-	 * @return respective response msg.
-	 */
-	
-	@PostMapping("/signup")
-	@Validate(apiName = "signup")
-	public ResponseEntity<JsonNode> signUp(@RequestBody JsonNode jsonNode){
-		log.debug("Entered into signUp");
-		
-		JsonNode responseNode = userService.signUp(jsonNode);
-		if(log.isDebugEnabled()) {
-			log.debug("exited from signUp");
-		}
-		return new ResponseEntity<>(responseNode,HttpStatus.OK);
-	}
-	
-	/***
-	 * This API used for user login.
-	 * 
-	 * @param jsonNode
-	 * @return respective msg.
-	 */
-	@PostMapping("/login")
-	@Validate(apiName = "login")
-	public ResponseEntity<JsonNode> login(@RequestBody JsonNode jsonNode){
-		if(log.isDebugEnabled()) {
-			log.debug("Entered into login");
-		}
-		JsonNode responseNode = userService.login(jsonNode);
-		if(log.isDebugEnabled()) {
-			log.debug("exited from login");
-		}
-		return new ResponseEntity<>(responseNode,HttpStatus.OK);
-	}
+    /***
+     * This API used for user login.
+     *
+     * @param jsonNode
+     * @return respective msg.
+     */
+    @PostMapping("/login")
+    @Validate(apiName = "login")
+    public ResponseEntity<JsonNode> login(@RequestBody JsonNode jsonNode) {
+        if (log.isDebugEnabled()) {
+            log.debug("Entered into login");
+        }
+        JsonNode responseNode = userService.login(jsonNode);
+        if (log.isDebugEnabled()) {
+            log.debug("exited from login");
+        }
+        return new ResponseEntity<>(responseNode, HttpStatus.OK);
+    }
 }
